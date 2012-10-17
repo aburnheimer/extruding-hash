609318f0aeca923f1cd6c0fa94b76558499011ca
Andrew Burnheimer <Andrew_Burnheimer@cable.comcast.com>
2012-10-17 18:07:31 -0400

    Add required Array last= operator

---
+++ spec/array_spec.rb
@@ -0,0 +1,13 @@
+require File.expand_path File.join(File.dirname(__FILE__), '../lib/extruding-hash/array')
+
+describe Array, "#last=" do
+  it "sets the last element to a new value" do
+    array = Array.new
+    array = [ 1, 3, 5 ]
+    array.last = 4
+    array.last.should eq(4)
+  end
+end
+
+# vim:set et ts=2 sts=2 sw=2 tw=72 wm=72 ai:
+
